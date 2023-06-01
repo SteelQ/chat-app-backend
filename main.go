5dfc884908ae5d8250fb841f66d9cbac7b3b1943
邱斌 <qb0926@gmail.com>
2023-06-01 14:48:25 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		// 允许所有的来源连接
+		return true
+	},
+}
+
+func main() {
+	http.HandleFunc("/ws", handleWebSocket)
+	log.Fatal(http.ListenAndServe(":3003", nil))
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// 升级 HTTP 连接为 WebSocket 连接
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to upgrade WebSocket connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// 读取客户端发送的消息
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Failed to read message from WebSocket:", err)
+			break
+		}
+
+		// 处理接收到的消息
+		log.Println("Received message:", string(msg))
+
+		// 发送响应消息给客户端
+		err = conn.WriteMessage(websocket.TextMessage, []byte("Hello, React Client!"))
+		if err != nil {
+			log.Println("Failed to send message to WebSocket:", err)
+			break
+		}
+	}
+}
