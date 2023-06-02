067cb5cd4a2939051fe9f4b2f151864fd420ff58
qiubin <qiubin@xiaoyouzi.com>
2023-06-02 11:09:01 +0800

    3000 => 3003

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var upgrader = websocket.Upgrader{
 
 func main() {
 	http.HandleFunc("/ws", handleWebSocket)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(":3003", nil))
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
